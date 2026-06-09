d0e3bd40bb17570cf7186fb88bfc1c47cd7589b2
Sihan Udayaratna <it24103532@my.sliit.lk>
2026-06-10 03:07:36 +0530

    switch to query_points API

--- vector_db.py
+++ vector_db.py
@@ -17,11 +17,11 @@ class QdrantStorage:
         self.client.upsert(self.collection, points=points)
 
     def search(self, query_vector, top_k: int = 5):
-        results = self.client.search(
+        response = self.client.query_points(
             collection_name=self.collection,
-            query_vector=query_vector,
+            query=query_vector,
             with_payload=True,
-            limit=top_k
+            limit=top_k,
         )
         contexts = []
         sources = set()
